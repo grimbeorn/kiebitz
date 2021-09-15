3ad5ef97121192c2b2b3ce9331c0514b67b0f4d9
grimbeorn <hernandomtz@gmail.com>
2021-09-14 22:46:38 -0500

    base project

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/kiebitz', function () {
+    return view('kiebitz');
+});
+
+Route::get('/proyectos', function () {
+    return view('proyectos');
+});
+
+Route::get('/servicios', function () {
+    return view('servicios');
+});
+
+Route::get('/presupuestos', function () {
+    return view('presupuestos');
+});
+
+// Route::get('/equipo', function () {
+//     return view('equipo');
+// });
+
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
+Route::get('/faqs', function () {
+    return view('faqs');
+});
+
+Route::post('/contact2', 'contact2Controller@send');
+
+Route::post('/presupuestos2', 'presupuestos2Controller@send');
+
+
